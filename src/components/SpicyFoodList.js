5c3a37d1e17bb7f39057a6c21997a5c1ab0bb1b2
Eve Kozina <eve.kozina@gmail.com>
2021-11-29 13:01:45 -0500

    commit tree 1a5a3b6e0bec2a2f868987556e5834399ede6c0e

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,16 +3,37 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  
+  const foodList = foods.map((food) => (
+    <li key={food.id} onClick={() => handleClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  ))
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
-
+  
+  function handleClick(id) {
+    const newFoodArray = foods.map ((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        }
+      } else {
+        return food;
+      }
+    })
+    setFoods(newFoodArray);
+  }
+  
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{foodList}</ul>
     </div>
   );
 }
